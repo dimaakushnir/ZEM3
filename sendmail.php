882d305198746708d9704fc1b685c5564c8c5ba1
dimaakushnir <101713021+dimaakushnir@users.noreply.github.com>
2023-08-07 16:31:29 +0300

    add changes

--- sendmail.php
+++ sendmail.php
@@ -11,25 +11,31 @@
     $mail->IsHTML(true);
 
     //От кото письма
-    $mail->setFrom('email1@gmail.com', 'Имя');
+    $mail->setFrom('email@gmail.com', 'LexPro');
     // Кому отправить
-    $mail->addAddress('email2@gmail.com');
+    $mail->addAddress('lexproua@ukr.net');
     // Тема письма
-    $mail->Subject = 'Тема письма"';
+    $mail->Subject = '';
 
 
     // Тело письма
-    $body = '<h1>Тело письма</h1>';
+    $body = '<h1>Заявка</h1>';
 
     if(trim(!empty($_POST['name']))){
-        $body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+        $body.='<p><strong>Від:</strong> '.$_POST['name'].'</p>';
     }
     if(trim(!empty($_POST['tel']))){
-        $body.='<p><strong>Tel:</strong> '.$_POST['tel'].'</p>';
+        $body.='<p><strong>Номер телефону:</strong> '.$_POST['tel'].'</p>';
     }
     if(trim(!empty($_POST['hidden']))){
-        $body.='<p><strong>Форма была отправлена с кнопки:</strong> '.$_POST['hidden'].'</p>';
+        $body.='<p><strong>Кнопка:</strong> '.$_POST['hidden'].'</p>';
     }
+    if(trim(!empty($_POST['hidden-title']))){
+        $body.='<p><strong>Заголовок:</strong> '.$_POST['hidden-title'].'</p>';
+    }
+    $body.='<p></p>';
+    $body.='<p>--</p>';
+    $body.='<p>Це повідомлення було відравлено з сайту LexPro (samobud.lexpro.com.ua)</p>';
 
     $mail->Body = $body;
 
